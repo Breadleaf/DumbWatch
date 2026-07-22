582522b440c214e4d54f411137def75811cd5047
Breadleaf <bkhnapa@gmail.com>
2026-07-22 08:22:41 -0700

    add PCB, CAD, and new firmware

---
+++ DumbWatch_Firmware/include/Tasks.hpp
@@ -0,0 +1,40 @@
+#ifndef TASKS_HPP
+#define TASKS_HPP
+
+#include <vector>
+#include <functional>
+
+#include "Types.hpp"
+
+namespace tasks
+{
+
+    struct BaseTaskData
+    {
+        const types::TimeMS_t interval = 0;
+        types::TimeMS_t lastRun = 0;
+    };
+
+    struct ScheduledTaskInterface
+    {
+        virtual void run(const types::TimeMS_t) = 0;
+    };
+
+    template <class T>
+    struct ScheduledTask : public ScheduledTaskInterface
+    {
+        T data;
+        std::function<void(const types::TimeMS_t)> function;
+
+        ScheduledTask(T _data, std::function<void(const types::TimeMS_t)> _function)
+            : data(_data), function(_function) {}
+
+        void run(const types::TimeMS_t currentTime) override
+        {
+            function(currentTime);
+        }
+    };
+
+} // tasks
+
+#endif // TASKS_HPP
---
+++ DumbWatch_Firmware/include/Types.hpp
@@ -0,0 +1,11 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+namespace types
+{
+
+    typedef unsigned long TimeMS_t;
+
+} // types
+
+#endif // TYPES_HPP
